914ac7421dfc494bd9b2777bc72812d16e3bc6fe
yu-mi <85780976+yumi0617@users.noreply.github.com>
2021-09-12 16:58:21 +0900

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+for(let count = 1; count <= 100; count++){
+
+    if(count % 15 == 0){
+        console.log(count + "回目　アホじゃない犬");
+    }
+            
+    else if(count % 3 == 0){
+        console.log(count + "回目　アホ");
+    }
+            
+    else if(count % 5 == 0){
+        console.log(count + "回目　犬");
+    }
+            
+    else{
+        console.log(count + "回目");
+        }
+}
